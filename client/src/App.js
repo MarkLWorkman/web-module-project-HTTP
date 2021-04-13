10ec1bb56ee56de52e3cbdbba6ad455aa097d518
unknown <calanhaga@aim.com>
2021-04-13 18:38:57 -0400

    App complete

--- client/src/App.js
+++ client/src/App.js
@@ -1,62 +1,98 @@
 import React, { useEffect, useState } from "react";
 
 import { Route, Switch, Redirect } from "react-router-dom";
-import MovieList from './components/MovieList';
-import Movie from './components/Movie';
+import MovieList from "./components/MovieList";
+import Movie from "./components/Movie";
 
-import MovieHeader from './components/MovieHeader';
+import MovieHeader from "./components/MovieHeader";
+import DeleteMovieModal from "./components/DeleteMovieModal";
+import AddMovie from "./components/AddMovie";
+import EditMovieForm from "./components/EditMovieForm";
+import FavoriteMovieList from "./components/FavoriteMovieList";
 
-import EditMovieForm from './components/EditMovieForm';
-import FavoriteMovieList from './components/FavoriteMovieList';
-
-import axios from 'axios';
+import axios from "axios";
 
 const App = (props) => {
   const [movies, setMovies] = useState([]);
   const [favoriteMovies, setFavoriteMovies] = useState([]);
 
-  useEffect(()=>{
-    axios.get('http://localhost:5000/api/movies')
-      .then(res => {
+  useEffect(() => {
+    axios
+      .get("http://localhost:5000/api/movies")
+      .then((res) => {
         setMovies(res.data);
       })
-      .catch(err => {
+      .catch((err) => {
         console.log(err);
       });
   }, []);
 
-  const deleteMovie = (id)=> {
-  }
+  const deleteMovie = (id) => {
+    setMovies(movies.filter((movie) => movie.id !== Number(id)));
+  };
 
-  const addToFavorites = (movie) => {
-    
-  }
+  const addToFavorites = (id) => {
+    console.log(id);
+
+    const newFavorite = movies.find((movie) => {
+      if (movie.id == id) {
+        return movie;
+      }
+    });
+
+    const checkIfMovieIsThere = favoriteMovies.find((favoriteMovie) => {
+      if (favoriteMovie.id == id) {
+        return favoriteMovie;
+      }
+    });
+
+    setFavoiteMovies(
+      JSON.stringify(checkIfMovieIsThere) === JSON.stringify(newFavorite)
+        ? [...favoriteMovies]
+        : [...favoriteMovies, newFavorite]
+    );
+  };
 
   return (
     <div>
       <nav className="navbar navbar-dark bg-dark">
-        <span className="navbar-brand" ><img width="40px" alt="" src="./Lambda-Logo-Red.png"/> HTTP / CRUD Module Project</span>
+        <span className="navbar-brand">
+          <img width="40px" alt="" src="./Lambda-Logo-Red.png" /> HTTP / CRUD
+          Module Project
+        </span>
       </nav>
 
       <div className="container">
-        <MovieHeader/>
+        <MovieHeader />
         <div className="row ">
-          <FavoriteMovieList favoriteMovies={favoriteMovies}/>
-        
+          <FavoriteMovieList
+            favoriteMovies={favoriteMovies}
+            setFavoriteMovies={setFavoriteMovies}
+          />
+
           <Switch>
             <Route path="/movies/edit/:id">
+              <EditMovieForm setMovie={setMovies} />
+            </Route>
+
+            <Route path="/movies/delete/:id">
+              <DeleteMovieModal {...props} deleteMovie={deleteMovie} />
+            </Route>
+
+            <Route path="/movies/add">
+              <AddMovie setMovie={setMovies} />
             </Route>
 
             <Route path="/movies/:id">
-              <Movie/>
+              <Movie {...props} addToFavorites={addToFavorites} />
             </Route>
 
             <Route path="/movies">
-              <MovieList movies={movies}/>
+              <MovieList {...props} movies={movies} />
             </Route>
 
             <Route path="/">
-              <Redirect to="/movies"/>
+              <Redirect to="/movies" />
             </Route>
           </Switch>
         </div>
@@ -65,6 +101,4 @@ const App = (props) => {
   );
 };
 
-
 export default App;
-
